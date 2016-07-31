8ae192412e7298d113f5e0bd8375fb9a5ff8fdee
Li Yi <denverdino@gmail.com>
2016-07-31 21:14:13 +0800

    Fix #1: Support the 1.12 GA version

--- provision-manager.sh
+++ provision-manager.sh
@@ -8,13 +8,14 @@ if ! which docker >/dev/null 2>&1; then
 	if [ USE_ALIYUN_MIRROR ]
 	then
 		sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/' /etc/apt/sources.list
-		curl -sSL http://aliyungo.oss-cn-hangzhou.aliyuncs.com/experimental-internet | sh
+		curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
 		echo DOCKER_OPTS="'--registry-mirror https://6udu7vtl.mirror.aliyuncs.com'" > /etc/default/docker
 		sudo service docker restart
 	else
-		curl -sSL https://experimental.docker.com/ | sh
+		curl -sSL https://get.docker.com/ | sh
 	fi
 	gpasswd -a vagrant docker
-	docker swarm init --listen-addr ${MANAGER_IP}:2377
+	docker swarm init --listen-addr ${MANAGER_IP}:2377 --advertise-addr ${MANAGER_IP}
+	docker swarm join-token -q worker >> /vagrant/worker_token
 fi
 
--- provision-worker.sh
+++ provision-worker.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-USE_ALIYUN_MIRROR=1
+#USE_ALIYUN_MIRROR=1
 
 MANAGER_IP=$1
 NODE_IP=$2
+TOKEN=`cat /vagrant/worker_token`
+
 
 if ! which docker >/dev/null 2>&1; then
 	if [ USE_ALIYUN_MIRROR ]
 	then
 		sed -i 's/archive.ubuntu.com/mirrors.aliyun.com/' /etc/apt/sources.list
-		curl -sSL http://aliyungo.oss-cn-hangzhou.aliyuncs.com/experimental-internet | sh
+		curl -sSL http://acs-public-mirror.oss-cn-hangzhou.aliyuncs.com/docker-engine/internet | sh -
 		echo DOCKER_OPTS="'--registry-mirror https://6udu7vtl.mirror.aliyuncs.com'" > /etc/default/docker
 		sudo service docker restart
 	else
-		curl -sSL https://experimental.docker.com/ | sh
+		curl -sSL https://get.docker.com/ | sh
 	fi
 	gpasswd -a vagrant docker
-	docker swarm join --listen-addr ${NODE_IP}:2377 ${MANAGER_IP}:2377
+	docker swarm join --listen-addr ${NODE_IP}:2377 --advertise-addr ${NODE_IP} --token=$TOKEN ${MANAGER_IP}:2377
 fi
 
 
